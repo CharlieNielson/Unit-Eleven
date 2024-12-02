eff8900302914f7609b6f31bea027a54298d3677
CharlieNielson <charlienielson@mail.weber.edu>
2024-12-02 15:14:30 -0700

    Update Connect Four.cpp

--- Connect Four.cpp
+++ Connect Four.cpp
@@ -4,10 +4,44 @@
 using namespace std;
 
 int main() {
-    //Player p;
-    Board b;
+    //cout << "debug";
 
-    b.display();
+    //Board b;
+    Player p("X", "O");
+
+    //system("clear");
+    //b.display();
+
+    bool gameEnd = false;
+    string winner;
+
+    while (gameEnd == false) {
+
+        cout << "Connect Four!" << endl;
+        p.display();
+        //cout << gameEnd << endl;//debug
+        cout << "Player one's turn." << endl;
+        p.play(p.menu(p.mark1), p.mark1, true);
+        gameEnd = p.hasWon(p.mark1);
+        winner = "Player One.";
+        system("clear");
+
+        if (gameEnd == false) {
+            cout << "Connect Four!" << endl;
+            p.display();
+            //cout << gameEnd << endl;//debug
+            cout << "Player two's turn." << endl;
+            p.play(p.menu(p.mark2), p.mark2, true);
+            gameEnd = p.hasWon(p.mark2);
+            winner = "Player Two.";
+            system("clear");
+        }
+    }
+    
+    cout << "Game End." << endl;
+    p.display();
+    cout << "Game over! Winner is " << winner;
+
 
     return 17;
 }
